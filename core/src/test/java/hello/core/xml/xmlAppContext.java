d84f7e73d123abb20c4258842c8a3dc4886d7022
yhyuntak <yhyuntak@gmail.com>
2023-03-05 14:10:50 +0900

    230305 업데이트

---
+++ core/src/test/java/hello/core/xml/xmlAppContext.java
@@ -0,0 +1,13 @@
+package hello.core.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+public class xmlAppContext {
+
+    @Test
+    void xmlAppContext(){
+        ApplicationContext genericApplicationContext = new GenericApplicationContext();
+    }
+}
